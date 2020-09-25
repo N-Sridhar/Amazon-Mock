d978d1bd2dbed6f3aa0a5fdcba68e85c60800ede
N-Sridhar <nsridharbtech@gmail.com>
2020-09-26 01:13:16 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAPSRP5kDEuMFs0yTDts3QKRreL1ThTu3w",
+    authDomain: "amzn-mock.firebaseapp.com",
+    databaseURL: "https://amzn-mock.firebaseio.com",
+    projectId: "amzn-mock",
+    storageBucket: "amzn-mock.appspot.com",
+    messagingSenderId: "849792183869",
+    appId: "1:849792183869:web:9d5a0e9103630983e08454",
+    measurementId: "G-GLC5DJW13D"
+  };
